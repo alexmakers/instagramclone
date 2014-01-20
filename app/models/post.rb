26b0b3db3daca9c453a57d213391067e2bac1a90
Alex Peattie <alexpeattie@gmail.com>
2014-01-20 15:02:00 +0000

    Uploads with S3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  has_attached_file :image, styles: { thumb: "300x300>" }
+  has_attached_file :image, 
+                    styles: { thumb: "300x300>" },
+                    storage: :s3,
+                    s3_credentials: {
+                      access_key_id: 'AKIAJPD4QQS6C5BNQF5Q',
+                      secret_access_key: Rails.application.secrets.s3_secret
+                    },
+                    bucket: 'instgram_clone_dev'
 end
